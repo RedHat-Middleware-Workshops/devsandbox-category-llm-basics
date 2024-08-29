edcbaa53886c760a47033703280c3544da139311
brunoNetId <bruno.meseguer@gmail.com>
2024-08-29 11:44:55 +0100

    fixes in camel snapshot available

--- camel/tools/l.sh
+++ camel/tools/l.sh
@@ -1,6 +1,4 @@
 camel run * web/* \
---maven-apache-snapshot-enabled=false \
 --logging-level=ERROR \
 --logging-category=dev.ai4j.openai4j=DEBUG \
---dep=dev.langchain4j:langchain4j-open-ai:0.33.0 \
---dep=com.github.javafaker:javafaker:1.0.2
+--dep=dev.langchain4j:langchain4j-open-ai:0.33.0,com.github.javafaker:javafaker:1.0.2
